65ff62e8bdde23bb7cd69f00865b19d34c12b45a
Ali Ghanavati <aliali3684@gmail.com>
2019-03-29 20:55:39 +0430

    Initial Commit

---
+++ Dowloader/Dowloader.h
@@ -0,0 +1,19 @@
+//
+//  Dowloader.h
+//  Dowloader
+//
+//  Created by Ali Ghanavati on 1/9/1398 AP.
+//  Copyright © 1398 AP Ali Ghanavati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dowloader.
+FOUNDATION_EXPORT double DowloaderVersionNumber;
+
+//! Project version string for Dowloader.
+FOUNDATION_EXPORT const unsigned char DowloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dowloader/PublicHeader.h>
+
+
